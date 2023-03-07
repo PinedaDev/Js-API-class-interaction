4ce2e00e7265ecad11abbd5a07e8e68e857cdd6a
abraham <abraham@pinedadev.com>
2023-03-07 10:43:40 +0200

    assignment 1 completed

--- assignment-1/index.js
+++ assignment-1/index.js
@@ -1,1 +1,14 @@
 // Your code here
+let i = 1;
+
+function numberSecuence() {
+    console.log(i)
+
+    if (i < 10 ** 6) {
+        i++;
+        setTimeout(numberSecuence, 0)
+    }
+}
+
+numberSecuence()
+
