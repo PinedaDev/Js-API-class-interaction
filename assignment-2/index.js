58a793676e8afc4b6a4ca0ccee11d9cee6ac1ad4
abraham <abraham@pinedadev.com>
2023-03-07 19:00:38 +0200

    fetch all data and by name

--- assignment-2/index.js
+++ assignment-2/index.js
@@ -1,1 +1,15 @@
-// Your code here
+class CountriesAPI {
+    #URL = "https://restcountries.com/v3.1"
+
+    async #fetchAllCountries() {
+        const response = await fetch(`${this.#URL}/all`);
+        const data = await response.json();
+        return data;
+    }
+
+    async #fetchByName(name) {
+        const response = await fetch(`${this.#URL}/name/${name}`);
+        const data = await response.json();
+        return data;
+    }
+}
